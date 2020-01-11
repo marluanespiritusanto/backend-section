be0b714fc3a319a709e8495d0c5f6cddaf1896e9
Marluan <marluanespiritusantoguerrero@gmail.com>
2020-01-11 16:46:05 +0100

    [ADD] jwt key added

--- src/config/index.js
+++ src/config/index.js
@@ -5,5 +5,6 @@ if (process.env.NODE_ENV !== "production") {
 module.exports = {
   PORT: process.env.PORT,
   MONGO_URI: process.env.MONGO_URI,
-  APPLICATION_NAME: process.env.APPLICATION_NAME
+  APPLICATION_NAME: process.env.APPLICATION_NAME,
+  JWT_SECRET: process.env.JWT_SECRET
 };
